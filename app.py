794768a8de7f53d8b35e6fa88644b039d8d210d1
NICOLAS HUMUD <nicolas@uc.cl>
2022-03-22 10:34:39 -0300

    inicializando repo

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/status", methods=['GET'])
+def status():
+    return "", 204
+
+
+@app.route("/info", methods=['GET'])
+def info():
+    return "", 200
+
+@app.route("/security", methods=['DELETE'])
+def security():
+    return "", 401
