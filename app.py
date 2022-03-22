e01e122d56289e392b36cebb786912b7b083a1b2
nhumud <nicolas@uc.cl>
2022-03-22 16:37:45 -0300

    texto en comillas simples

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ app = Flask(__name__)
 
 @app.route("/status", methods=['GET'])
 def status():
-    return "", 204
+    return ('', 204)
 
 
 @app.route("/info", methods=['GET'])
 def info():
-    return "", 200
+    return ('', 200)
 
 
 @app.route("/security", methods=['DELETE'])
 def security():
-    return "", 401
+    return ('', 401)
